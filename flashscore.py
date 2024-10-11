c92464802e37d22182c86ccd33ed77200a9b0035
Dmitry <luka-jr@yandex.ru>
2024-10-11 23:27:45 +0300

    4

--- flashscore.py
+++ flashscore.py
@@ -17,15 +17,17 @@ config.read("flashscore.cfg")
 # k_country = config['Keys']['country']
 # username = config['Keys']['username']
 
-feed = 'ru'
-url = f'https://flashscore.com/{feed}'
+feed = 'f_1_0_3_ru_1'
+url = f'https://46.flashscore.ninja/46/x/feed/{feed}'
 
 def main():
-    # feed = 'f_1_-1_3_ru_5'
+    # feed = 'f_1_0_3_ru_1'     #   список игр
     # url = f'https://d.flashscore.ru.com/x/feed/{feed}'
     # feed = 'r_1_1'
     # url = f'https://46.flashscore.ninja/46/x/feed/{feed}'
     # feed = 'r_4_1'
+    # url = f'https://2.flashscore.ninja/802/x/feed/{feed}'
+    # feed = 'fo_1_0_3_ru_1_0'  #   КЭФы
     # url = f'https://2.flashscore.ninja/802/x/feed/{feed}'
     response = requests.get(url=url, headers=headers)
     r_text = response.text
@@ -33,12 +35,19 @@ def main():
         prepars(r_text)
 
 def is_data(r_text):
-    with open(feed, 'w', encoding='utf8') as outfile:
-        print(r_text, file=outfile)
-    soup = BeautifulSoup(r_text, 'lxml')
+    if r_text.find('<!',0)==1:  #   HTML
+        with open(feed+'html', 'w', encoding='utf8') as outfile:
+            print(r_text, file=outfile)
+        return False
+        # soup = BeautifulSoup(r_text, 'lxml')
     return True
 
 def prepars(r_text):
+    if r_text.find('SA',0)==-1:   #   Предположительно блок данных
+        with open(feed, 'w', encoding='utf8') as outfile:
+            print(r_text, file=outfile)
+        return False
+
     data = r_text.split('¬')
 
     data_list = [{}]
@@ -46,6 +55,7 @@ def prepars(r_text):
     for item in data:
         key = item.split('÷')[0]
         value = item.split('÷')[-1]
+        value = checkdate(value)
 
         if '~' in key:
             data_list.append({key: value})
@@ -64,6 +74,12 @@ def prepars(r_text):
 
     with open(feed+'.json', 'w', encoding='utf8') as outfile:
         json.dump(data_list, outfile, ensure_ascii=False, indent=2)
+
+def checkdate(value):
+    if len(value)==10 and value.isdigit():
+        if int(value)>1234567890:
+            value = str(datetime.fromtimestamp(int(value)))
+    return value
 
 
 if __name__ == '__main__':
